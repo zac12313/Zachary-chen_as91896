224396f867ea0853679977f14152c49b5f24b26b
23147 <23147@students.mrgs.school.nz>
2026-05-01 12:31:33 +1200

    tk added

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import tkinter as tk
+from tkinter import font
 root= tk.Tk()
-label=tk.Label(root, text="bomboclatt quiz", width=67,bg="red",font="arial",).grid(row=0, column=5)
+
+my_font = font.Font(family="open sans", size=16, weight="bold")
+
+label=tk.Label(root, text="bomboclatt quiz",bg="yellow",font=my_font).grid(row=0, column=1)
 
 
-tk.Label(root, text="First Name").grid(row=6, column=10)
-tk.Label(root, text="Last Name").grid(row=7, column=10)
+tk.Label(root, text="First Name",bg="pink").grid(row=6, column=0)
+tk.Label(root, text="Last Name",bg="pink").grid(row=7, column=0)
 
 entry1 = tk.Entry(root)
 entry2 = tk.Entry(root)
 
-entry1.grid(row=6, column=10)
-entry2.grid(row=7, column=10)
+entry1.grid(row=6, column=1)
+entry2.grid(row=7, column=1)
 
 root.title("Gym quiz")
 
 button = tk.Button(root,text="Exit", width=12,command=root.destroy).grid(row=0)
 
 
-
+root.configure(bg="light blue")
 
 
 
